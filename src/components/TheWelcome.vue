105a4c7a4fa0385bd8c09162f10c60c98ef932a6
wyfan <wyfbdm18@gmail.com>
2024-04-12 11:56:05 +0800

    04121156

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -16,7 +16,7 @@ import SupportIcon from './icons/IconSupport.vue'
 
     Vue’s
     <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+    provides you with all information I need to get started.
   </WelcomeItem>
 
   <WelcomeItem>
